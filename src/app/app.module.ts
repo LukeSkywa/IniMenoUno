b589afe1c161733165ae21b3d8096982e8623c17
Riccardo-2000 <you@example.comrizzini.riccardo.marconi@gmail.com>
2020-04-24 15:51:41 +0200

    feedback con invio mail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { FilmDetailComponent } from './components/film-detail/film-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -33,13 +35,13 @@ import { FilmDetailComponent } from './components/film-detail/film-detail.compon
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,  
     BrowserAnimationsModule,
     MatButtonToggleModule, 
     MatIconModule,
     FormsModule,
+    HttpClientModule,
   ],
   
   providers: [],
